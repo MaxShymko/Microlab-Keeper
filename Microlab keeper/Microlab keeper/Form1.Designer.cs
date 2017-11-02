a26d265b54d0227c5f85c2f70ac3fdf32054062e
ProlificStark <shimko.max@mail.ru>
2017-11-02 12:34:05 +0300

    Add project

---
+++ Microlab keeper/Microlab keeper/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Microlab_keeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button_loadCode = new System.Windows.Forms.Button();
+            this.label_status = new System.Windows.Forms.Label();
+            this.button_start = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_addCode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Code path:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox2.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.Location = new System.Drawing.Point(90, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(348, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Choose your code file";
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // button_loadCode
+            // 
+            this.button_loadCode.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_loadCode.Location = new System.Drawing.Point(15, 50);
+            this.button_loadCode.Name = "button_loadCode";
+            this.button_loadCode.Size = new System.Drawing.Size(75, 23);
+            this.button_loadCode.TabIndex = 3;
+            this.button_loadCode.Text = "Load code";
+            this.button_loadCode.UseVisualStyleBackColor = true;
+            this.button_loadCode.Click += new System.EventHandler(this.button_loadCode_Click);
+            // 
+            // label_status
+            // 
+            this.label_status.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_status.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label_status.Location = new System.Drawing.Point(388, 52);
+            this.label_status.Name = "label_status";
+            this.label_status.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label_status.Size = new System.Drawing.Size(50, 18);
+            this.label_status.TabIndex = 4;
+            this.label_status.Text = " - 0000";
+            this.label_status.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button_start
+            // 
+            this.button_start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_start.Location = new System.Drawing.Point(96, 50);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 5;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(358, 51);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(270, 52);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(85, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Start adress:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button_addCode
+            // 
+            this.button_addCode.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_addCode.Location = new System.Drawing.Point(177, 50);
+            this.button_addCode.Name = "button_addCode";
+            this.button_addCode.Size = new System.Drawing.Size(75, 23);
+            this.button_addCode.TabIndex = 8;
+            this.button_addCode.Text = "Update";
+            this.button_addCode.UseVisualStyleBackColor = true;
+            this.button_addCode.Click += new System.EventHandler(this.button_addCode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(450, 84);
+            this.Controls.Add(this.button_addCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.button_loadCode);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Microlab keeper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_loadCode;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_addCode;
+    }
+}
+
---
+++ Microlab keeper/Microlab keeper/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Microlab_keeper
+{
+    public partial class Form1 : Form
+    {
+        // WinAPI
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool PostMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        static extern int LoadKeyboardLayout(string pwszKLID, uint Flags);
+
+
+        const int WM_KEYDOWN = 0x100,
+            ENTER = 0x0D, // Точка
+            VIRGULE = 0xBC, // Запятая
+            KeyDownDelay = 150, // Задержка после нажатия клавиши (если меньше 150 мс - есть сбои)
+            startAdressDefault = 0x0100;
+
+
+        string[] code = null; // Код
+        bool isFileChoosen = false; // Загружен ли файл
+        int startAdress = startAdressDefault;
+        int lastAdress = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Int2Hex(startAdress);
+            openFileDialog1.FileName = "";
+        }
+
+        /// <summary>
+        /// Получить ид процесса microlab
+        /// </summary>
+        /// <returns></returns>
+        private Process GetMicrolab()
+        {
+            Process microlabProccess;
+            try
+            {
+                microlabProccess = Process.GetProcessesByName("microlab")[0];
+            }
+            catch
+            {
+                return null;
+            }
+
+            // Изменить раскладку в окне microlab на EN
+            PostMessage(microlabProccess.MainWindowHandle, 0x50, 1, LoadKeyboardLayout("00000409", 1));
+
+            return microlabProccess;
+        }
+
+        /// <summary>
+        /// Загрузка кода из файла. Считывает первые 2 символа и заносит в массив, если символ: 0-9|A-B
+        /// </summary>
+        /// <returns></returns>
+        private string[] LoadCode()
+        {
+            if (!File.Exists(openFileDialog1.FileName))
+                return null;            
+            var tmp = File.ReadAllLines(openFileDialog1.FileName).
+                Where(n => 
+                    !string.IsNullOrEmpty(n) && 
+                    new Regex("^[A-F0-9/-]{2}", RegexOptions.IgnoreCase).IsMatch(n)).
+                ToArray();
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                if(tmp[i][0] == '-' && tmp[i][1] == '-')
+                {
+                    Array.Resize<string>(ref tmp, i);
+                    break;
+                }
+                else
+                    tmp[i] = tmp[i].ToUpper();
+            }
+            return tmp;
+        }
+
+        /// <summary>
+        /// Выбрать файл
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.FileName;
+                isFileChoosen = true;
+            }
+        }
+       
+        /// <summary>
+        /// Перевести из string(hex) в int
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns></returns>
+        private int Hex2Int(string hex)
+        {
+            int num = 0;
+            try
+            {
+                num = Convert.ToInt32(hex, 16);
+            }
+            catch
+            {
+                return 0;
+            }
+            return num;
+        }
+
+        /// <summary>
+        /// Загрузить код в microlab
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_loadCode_Click(object sender, EventArgs e)
+        {
+            if (!isFileChoosen)
+            {
+                MessageBox.Show("Выберите файл с кодом программы!");
+                return;
+            }
+            code = LoadCode();
+            if (code == null)
+            {
+                MessageBox.Show("Код программы не найден!");
+                return;
+            }
+            var microlabProccess = GetMicrolab();
+            if (microlabProccess == null)
+            {
+                MessageBox.Show("microlab не запущен!");
+                return;
+            }
+
+            startAdress = Hex2Int(textBox1.Text);
+            if (startAdress == 0)
+            {
+                MessageBox.Show("Неверный адрес!");
+                return;
+            }
+
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, ENTER, "");
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, '0', "");
+
+            //Start adress
+            SendAdress(microlabProccess.MainWindowHandle, Int2Hex(startAdress));
+
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, VIRGULE, "");
+
+            int i;
+            for (i = 0; i < code.Length; i++)
+            {
+                SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, code[i][0], "");
+                SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, code[i][1], "");
+                SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, VIRGULE, "");
+            }
+            SendMessage(microlabProccess.MainWindowHandle, WM_KEYDOWN, ENTER, "");
+            lastAdress = startAdress + i - 1;
+
+            label_status.Text = " - " + Int2Hex(lastAdress);
+
+            MessageBox.Show("Код программы загружен. Адрес: " + Int2Hex(startAdress) + "h - " + Int2Hex(lastAdress) + "h");
+        }
+
+        /// <summary>
+        /// Запустить код
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            var microlabProccess = GetMicrolab();
+            if (microlabProccess == null)
+            {
+                MessageBox.Show("microlab не запущен!");
+                return;
+            }
+            else if (lastAdress == 0)
+            {
+                MessageBox.Show("Код программы не загружен!");
+                return;
+            }
+
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, ENTER, "");
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, '2', "");
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, '0', "");
+
+            SendAdress(microlabProccess.MainWindowHandle, Int2Hex(startAdress));
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, VIRGULE, "");
+            SendAdress(microlabProccess.MainWindowHandle, Int2Hex(lastAdress));
+
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, ENTER, "");
+        }
+
+        /// <summary>
+        /// Добавить новый код
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_addCode_Click(object sender, EventArgs e)
+        {
+            if (!isFileChoosen)
+            {
+                MessageBox.Show("Выберите файл с кодом программы!");
+                return;
+            }
+            if (code == null)
+            {
+                MessageBox.Show("Загрузите исходный код (Load code)");
+                return;
+            }
+            string[] newCode = LoadCode();
+            if (newCode == null)
+            {
+                MessageBox.Show("Код программы не найден!");
+                return;
+            }
+            var microlabProccess = GetMicrolab();
+            if (microlabProccess == null)
+            {
+                MessageBox.Show("microlab не запущен!");
+                return;
+            }
+            int oldStartAdress = startAdress;
+            startAdress = Hex2Int(textBox1.Text);
+            if (startAdress == 0)
+            {
+                MessageBox.Show("Неверный адрес!");
+                return;
+            }
+
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, ENTER, "");
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, '0', "");
+            SendAdress(microlabProccess.MainWindowHandle, Int2Hex(startAdress));
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, VIRGULE, "");
+
+            int i = 0, changes = 0;
+            for (int oldI; i < newCode.Length; i++)
+            {
+                oldI = i + startAdress - oldStartAdress;
+                if (oldI >= code.Length || oldI < 0 || String.Compare(newCode[i], code[oldI], true) != 0)
+                {
+                    SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, newCode[i][0], "");
+                    SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, newCode[i][1], "");
+                    changes++;
+                }
+
+                SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, VIRGULE, "");
+            }
+            SendMessageWithDelay(microlabProccess.MainWindowHandle, WM_KEYDOWN, ENTER, "");
+            code = newCode;
+
+            lastAdress = startAdress + code.Length - 1;
+            label_status.Text = " - " + Int2Hex(lastAdress);
+
+            MessageBox.Show("Код программы обновлен. Строк изменено: " + changes + "\nАдрес: " + Int2Hex(startAdress) + "h - " + Int2Hex(lastAdress) + "h");
+        }
+
+        /// <summary>
+        /// Переводит int в string(hex). Длина - 4 символа
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private static string Int2Hex(int num)
+        {
+            byte[] data = BitConverter.GetBytes(num);
+            Array.Reverse(data);
+            return new Regex("-").Replace(BitConverter.ToString(data), "").Remove(0, 4);
+        }
+
+        /// <summary>
+        /// Ввести начальный адрес
+        /// </summary>
+        /// <param name="prog"></param>
+        /// <param name="adress"></param>
+        private void SendAdress(IntPtr prog, string adress)
+        {
+            for (int i = 0; i < adress.Length; i++)
+            {
+                SendMessageWithDelay(prog, WM_KEYDOWN, adress[i], "");
+            }
+        }
+
+        /// <summary>
+        /// SendMessage с задержкой
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="uMsg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        private void SendMessageWithDelay(IntPtr hWnd, int uMsg, int wParam, string lParam)
+        {
+            SendMessage(hWnd, uMsg, wParam, lParam);
+            Thread.Sleep(KeyDownDelay);
+        }
+    }
+}
